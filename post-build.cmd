ba225846e67f806a59592952350ac0c90c7d04d3
Rik Essenius <essenius@users.noreply.github.com>
2023-02-12 20:30:38 +0100

    Corrected post-build script to include library folder

--- post-build.cmd
+++ post-build.cmd
@@ -5,9 +5,16 @@ rem The Compress-Archive command can't deal very well with selective addition of
 if exist "%temp%\%project%" rmdir "%temp%\%project%" /s /q
 mkdir "%temp%\%project%\src"
 copy "%basePath%src\*.h" "%temp%\%project%\src" 2>nul
+copy "%basePath%src\*.cpp" "%temp%\%project%\src" 2>nul
+mkdir "%temp%\%project%\examples"
+xcopy /e "%basePath%examples" "%temp%\%project%\examples"
+copy "%basePath%keywords.txt" "%temp%\%project%"
+copy "%basePath%library.properties" "%temp%\%project%"
+copy "%basePath%LICENSE" "%temp%\%project%"
+copy "%basePath%README.md" "%temp%\%project%"
 rem Create target folder if needed
 if not exist "%basePath%publish" mkdir "%basePath%publish"
-rem Create the ZIP file with only the content we need
-powershell.exe -Command Compress-Archive -Path '%temp%\SafeCString\src', '%basePath%examples','%basePath%keywords.txt','%basePath%library.properties','%basePath%LICENSE', '%basePath%README.md' -DestinationPath '%basePath%publish\%project%.zip' -Force
+rem Create the ZIP file from the temp structure
+powershell.exe -Command Compress-Archive -Path '%temp%\SafeCString' -DestinationPath '%basePath%publish\%project%.zip' -Force
 rem Clean up the temporary folder
 rmdir "%temp%\%project%" /s /q
